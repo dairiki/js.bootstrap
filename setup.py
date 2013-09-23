fd0db54e57a601176cdcd4fdf2fff154795901b8
Jeff Dairiki <dairiki@dairiki.org>
2013-09-23 12:07:31 -0700

    Pull upstream master (229469e Mon Sep 23 09:03:16 2013 -0700)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '3.0.0.post1'
+version = '3.0.0.post2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
